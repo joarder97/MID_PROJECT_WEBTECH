3996ea7f59e20cac4ca3a6e6e80349792dede6d3
joarder97 <masudul.pallab99@gmail,com>
2020-08-12 04:04:42 +0600

    Fle i/o included

--- cookies.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!empty($_POST["remember"])) {
-	setcookie ("username",$_POST["id"],time()+ 3600);
-	setcookie ("password",$_POST["password"],time()+ 3600);
-} else {
-	setcookie("username","");
-	setcookie("password","");
-}
-?>
--- home.php
+++ home.php
@@ -2,7 +2,8 @@
 	session_start();
 	include 'server.php';
 	if(isset($_SESSION['status'])){
-		if($_SESSION['status'] = "ok"){
+		if($_SESSION['status'] = "ok") 
+		{
 
 ?>
 <!DOCTYPE html>
@@ -13,26 +14,61 @@
 <body>
 
 
-	
+	<form method="post">
             
-            <p> 
+            <h2> 
                 Welcome  
                 
                     <?php echo $_SESSION['id'] ?> 
                
-            </p>
-            
+            </h2>
+
+            <p>Enter User Information: (Name - Payment Status)    <input type="text" name="infoNameUser" value="">  <input type="text" name="infoPaymentStatus" value=""> <input type="submit" name="infoSubmit">
+             <input type="submit" name="show" value="View Info"></p>
+
+            <?php
             
 
+            if(isset($_POST["infoSubmit"]))
+ 		 	{
+ 		 	$name = $_POST["infoNameUser"];
+            $status = $_POST["infoPaymentStatus"];
+            	if(empty($name) || empty($status))
+            	{
+            		echo "Empty fields found. Please try again.";
+            	}else{
+	            $file = fopen("userinfo.txt",'a');
+	            $user = $name."   |   ".$status.PHP_EOL;
+	            fwrite($file, $user); 
+	            echo "User Information Added";       
+       	 }
+		}
+
+		if(isset($_POST["show"])){
+				echo "USERNAME ||| PAYMENT STATUS<br>";
+				$fh = fopen("userinfo.txt", 'r');
+				$content = fread($fh, 25000);
+				echo nl2br($content);
+			}
+
+             ?>
+
+            
+            <hr> 
+
 	<button><a href="logout.php">Logout</a></button>
+	</form>
 </body>
 </html>
 <?php
+
 	}else{
 	header('location: login.php');
 		}
-	}else{
-		header('location: login.php');
+	}
+	
+		else{
+			header('location: login.php');
 	}
 	
 
